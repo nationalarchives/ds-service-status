d94a410d7b4c85522b9caf10039744b8daf813ef
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-08-11 15:30:49 +0100

    Handle unreachable API

--- app/status/routes.py
+++ app/status/routes.py
@@ -1,11 +1,11 @@
-from app.lib.api import JSONAPIClient
+from app.lib.api import JSONAPIClient, ResourceNotFound
 from app.lib.cache import cache, cache_key_prefix
 from app.status import bp
 from flask import current_app, render_template
 
 
 @bp.route("/")
-@cache.cached(key_prefix=cache_key_prefix, timeout=15)
+@cache.cached(key_prefix=cache_key_prefix)
 def index():
     uptime_kuma_api_url = current_app.config["UPTIME_KUMA_API_URL"]
     uptime_kuma_status_page_slug = current_app.config["UPTIME_KUMA_STATUS_PAGE_SLUG"]
@@ -16,6 +16,10 @@ def index():
         current_app.logger.critical("UPTIME_KUMA_STATUS_PAGE_SLUG not set")
         raise Exception("UPTIME_KUMA_STATUS_PAGE_SLUG not set")
     client = JSONAPIClient(uptime_kuma_api_url)
-    data = client.get(f"status-page/{uptime_kuma_status_page_slug}")
-    heartbeats = client.get(f"status-page/heartbeat/{uptime_kuma_status_page_slug}")
-    return render_template("status/index.html", data=data, heartbeats=heartbeats)
+    try:
+        data = client.get(f"status-page/{uptime_kuma_status_page_slug}")
+        heartbeats = client.get(f"status-page/heartbeat/{uptime_kuma_status_page_slug}")
+        return render_template("status/index.html", data=data, heartbeats=heartbeats)
+    except ResourceNotFound as e:
+        current_app.logger.error(f"Failed to render status page: {e}")
+        return render_template("errors/api.html"), 502
